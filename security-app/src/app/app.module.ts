c70a85911ae09d76b851c5dd814f4f534448823c
gadyez <gady@payboxapp.com>
2017-06-30 11:52:15 +0300

    remove routing

--- security-app/src/app/app.module.ts
+++ security-app/src/app/app.module.ts
@@ -2,19 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
-import {SecurityRouteService} from 'services/security-route.service';
+import { SecurityRouteService } from 'services/security-route.service';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DataWindowComponent } from './data-window/data-window.component';
-import { Routes,RouterModule } from '@angular/router';
-
-const appRoutes: Routes = [
-  {path: '', redirectTo: '/main', pathMatch:'full'},
-  {path: 'main', component: AppComponent}
-];
 
 @NgModule({
-  exports: [RouterModule],
   declarations: [
     AppComponent,
     MenuComponent,
@@ -25,7 +18,6 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     JsonpModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [SecurityRouteService],
   bootstrap: [AppComponent]
